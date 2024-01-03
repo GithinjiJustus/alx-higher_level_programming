fd69086f15d1b26e929160b5ff4079dcb0bd0506
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-03 04:37:12 -0800

    I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for char in range(26):  #characters#
-    print("{:s}".format(chr(char + ord("a"))), end="") #ASCII values interpreted#
+for char in range(26):  # Iterate through each character in the range 0 to 25#
+    print("{:s}".format(chr(char + ord("a"))), end="")  # Print the character#
