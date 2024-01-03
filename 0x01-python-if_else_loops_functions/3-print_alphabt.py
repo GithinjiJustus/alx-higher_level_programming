e7fd183115eeef7a335bda68bca8c1fd739a3494
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-03 04:42:41 -0800

    This Python script will output all the lowercase letters from a to z

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-for char in range(26):  #ASCII value#
-    if char != 4 and char != 16:  #ASCII value denoted#
-        print("{:s}".format(chr(char + ord("a"))), end="") #print ASCII#
+# Print lowercase alphabet letters excluding positions 4 and 16#
+for char in range(26):
+    # Check if the character position is not 4 and not 16#
+    if char != 4 and char != 16:
+        # Convert the numerical value to its corresponding ASCII character#
+        # Add the ASCII value of 'a' to the current value of 'char'#
+        # Print the character without a newline (end="")#
+        print("{:s}".format(chr(char + ord("a"))), end="")
 
