3976039e7a1933ec84b41c74f18ac5a46d39fdf7
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-03 05:05:09 -0800

    what is that

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for char in range(26):
-    # Check if the current character is not 'e' (char 4) & not 'q' (char 16)#
-    if char != 4 and char != 16:
-        # Print the character corresponding to the current number#
-        print("{:s}".format(chr(char + ord("a"))), end="")
+# Loop through numbers from 0 to 99
+for num in range(100):
+    # Format the number with two digits (leading zero if needed)
+    # If it's the last number, print with a newline
+    print("{:02d}".format(num), end=", " if num != 99 else "\n")
