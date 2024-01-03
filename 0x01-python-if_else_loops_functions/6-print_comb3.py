fa62fbd32611c8348ba4550d86da7921bf3b6def
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-03 05:22:34 -0800

    i have an idea

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    # Check if the tens digit is not equal to the units digit
-    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
-        # Print the pair of numbers (tens and units digits)
-        print("{}{}".format(int(i / 10), i % 10), end="")
-        # Check if it's not the number 89 to avoid adding a comma
-        if (i != 89):
-            print(", ", end="")
-            # Print a new line after all pairs are printed
-            print("")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
