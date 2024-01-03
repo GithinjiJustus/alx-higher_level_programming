73b1ce758db9fe764c28dd5f51cfd5f60e19a7cc
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-03 04:51:40 -0800

    C is easier

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -3,4 +3,4 @@
 # Loop through numbers from 0 to 99 (inclusive)
 for num in range(99 + 1):
     # Print the number in decimal and hexadecimal format
-    print(f"{num} = 0x{num:x}")
+    print("{:d} = {}".format(num, hex(num)))
