5ac621479c57e1ce56630b12ac9ab9e5f063765a
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-03 04:58:05 -0800

    C is easier

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Loop through numbers from 0 to 100(inclusive)
-for num in range(100):
+# Loop through numbers from 0 to 99(inclusive)
+for num in range(99):
     # Print the number in decimal and hexadecimal format
     print("{} = {}".format(num, hex(num)))
