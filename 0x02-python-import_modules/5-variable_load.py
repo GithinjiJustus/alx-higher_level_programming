c195ee3bcef014395eeb2974775e03015ac5a218
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-04 07:29:48 -0800

    new

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+if __name__ == "__main__":
+    """Prints the value of variable 'a' imported from variable_load_5."""
+    from variable_load_5 import a
+
+    # Printing the value of variable 'a'
+    print(a)
