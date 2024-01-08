86abf3106f41cf06a602c291c493145e8722deb7
GithinjiJustus <justusgithinji257@gmail.com>
2024-01-08 04:50:11 -0800

    minor changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    #  Iterate through each row in the matrix
     for row in matrix:
-        #  Iterate through each column in the row
         for col in row:
-            #  Print the integer with formatting
             print("{:d}".format(col), end=" " if col != row[-1] else "")
-            #  Print a space after the integer if it's not last element in row
-            #  Move to the next line after printing all columns in the row
-            print ()
+        print()            
