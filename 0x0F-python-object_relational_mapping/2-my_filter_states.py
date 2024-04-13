3b0cbfd99c557f3f4499b8a281aff24ded3e6b91
GithinjiJustus <justusgithinji257@gmail.com>
2024-04-13 11:02:18 -0700

    new

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+in the `states` table of `hbtn_0e_0_usa` where `name`
+matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(search))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
