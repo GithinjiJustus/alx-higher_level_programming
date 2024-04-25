ba923395ff0b07db8e54faf01817c9fbba294743
GithinjiJustus <justusgithinji257@gmail.com>
2024-04-25 01:47:39 -0700

    curl

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script takes a URL as input, sends a GET request to that URL
+
+curl -sL "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script takes a URL as input, sends a POST request to the URL with specific parameters, and displays the body of the response
+
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+
